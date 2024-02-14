23b2b6e82878c408ba38121e5f8a7c33d24d1386
sushant <sushantlama732@gmail.com>
2024-02-14 18:25:42 +0545

    fixed thankyou page height

--- src/Pages/Thankyou.tsx
+++ src/Pages/Thankyou.tsx
@@ -5,10 +5,13 @@ const Thankyou = () => {
   return (
     <div>
         <Navbar/> 
+        <div className="min-h-[100vh]">
         <h3 className=" text-2xl text-center py-10 font-extrabold">
               {" "}
               Thank you your order is placed
             </h3>
+        </div>
+       
        
     </div>
   )
